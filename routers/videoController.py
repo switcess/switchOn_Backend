3feb02084927c5f6b10146c3704ce2fac25a9272
ajeong <ajung7038@naver.com>
2024-08-25 15:42:17 +0900

    Fix: 반환값 변경 #5

--- routers/videoController.py
+++ routers/videoController.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Depends # type: ignore
-from sqlalchemy.orm import Session # type: ignore
-import schemas.cctvCreateRequest as cctvCreateRequest, domain.cctvEntity as cctvEntity
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+import domain.cctvEntity as cctvEntity
 import schemas.videoFindAllRequest as videoFindAllRequest
 import schemas.videoFindAllResponse as videoFindAllResponse
 from database import SessionLocal, engine
 from service import videoService
+from typing import List
 
 #DB 테이블 생성
 cctvEntity.Base.metadata.create_all(bind=engine)
@@ -23,11 +24,7 @@ router = APIRouter(
     tags=["video"]
 )
 
-@router.post("/", response_model=videoFindAllResponse.VideoFindAllResponse)
-# @router.post("/")
+@router.post("/", response_model=List[videoFindAllResponse.VideoFindAllResponse])
 def findAllVideo(request: videoFindAllRequest.VideoFindAllRequest, db: Session = Depends(get_db)):
     response = videoService.findAll_video(db=db, request=request)
-    if response is None:
-        return {"detail": "해당 날짜에 비디오가 존재하지 않습니다."}
     return response
-    # return "돼라."
--- service/videoService.py
+++ service/videoService.py
@@ -1,23 +1,17 @@
 from sqlalchemy.orm import Session
-from fastapi import HTTPException
-
-# 기존에 생성한 모델과 스키마 불러오기
-# from ... import models, schemas
 import domain.cctvEntity as cctvEntity, schemas.videoFindAllRequest as videoFindAllRequest
 import domain.cctvEntity as cctvEntity, schemas.videoFindAllResponse as videoFindAllResponse
 
-from datetime import datetime
-
 def findAll_video(db: Session, request: videoFindAllRequest.VideoFindAllRequest):
-    video = db.query(cctvEntity.CCTV).filter(cctvEntity.CCTV.date == request.date).first()
+    videos = db.query(cctvEntity.CCTV).filter(cctvEntity.CCTV.date == request.date).all()
     
-    if not video:
-        raise HTTPException(status_code=404, detail="CCTV item not found")
-    
-    videoResponse = videoFindAllResponse.VideoFindAllResponse(
+    videoList = [
+        videoFindAllResponse.VideoFindAllResponse(
+            id=video.id,
             date=video.date,
             time=video.time,
             actions=video.actions
         )
-    
-    return videoResponse
+        for video in videos
+    ]
+    return videoList
